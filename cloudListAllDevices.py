db7d75fdf94a18415634d4d460b16ad8daea1a70
andrebolzan <37314712+andrebolzan@users.noreply.github.com>
2024-09-24 18:06:55 -0300

    Update cloudListAllDevices.py

--- cloudListAllDevices.py
+++ cloudListAllDevices.py
@@ -1,6 +1,7 @@
+#!/usr/bin/env python3
 import argparse
-import tinytuya # pip install tinytuya
-from tabulate import tabulate #pip install tabulate
+import tinytuya  # pip install tinytuya
+from tabulate import tabulate  # pip install tabulate
 
 # Configurar o argparse para receber os parâmetros via linha de comando
 parser = argparse.ArgumentParser(description='Obtenha o status de um dispositivo Tuya.')
@@ -8,6 +9,7 @@ parser = argparse.ArgumentParser(description='Obtenha o status de um dispositivo
 # Definindo os argumentos que o script aceitará
 parser.add_argument('apikey', type=str, help='key da API Tuya')
 parser.add_argument('apiSecret', type=str, help='Secret da API')
+parser.add_argument('--geralocalkey', action='store_true', help='Gera um arquivo localKey.txt com id e key (opcional)')
 
 # Analisa os argumentos fornecidos via linha de comando
 args = parser.parse_args()
@@ -15,21 +17,33 @@ args = parser.parse_args()
 # Connect to Tuya Cloud
 c = tinytuya.Cloud(
     apiRegion="us",
-    apiKey=args.apikey,  # Corrigido: adicionada vírgula
+    apiKey=args.apikey,
     apiSecret=args.apiSecret
 )
 
 # Display list of devices
 devices = c.getdevices()
 
-# Seleciona as colunas que deseja exibir
-headers = ["name", "id", "key", "mac", "category", "product_name", "model"]
+# Seleciona os campos desejados
+keys = ["id", "key"]
+local_keys = []
 
-# Prepara a lista de dispositivos com os campos desejados
-device_list = [[device.get(header, '') for header in headers] for device in devices]
+# Coleta os id's e keys dos dispositivos
+for device in devices:
+    device_id = device.get("id")
+    device_key = device.get("key")
+    if device_id and device_key:
+        local_keys.append(f"{device_id}|{device_key}")
 
-# Ordena a lista pela coluna "name" (primeiro campo em cada sublista)
-device_list_sorted = sorted(device_list, key=lambda x: x[0])
-
-# Exibe a tabela ordenada
-print(tabulate(device_list_sorted, headers=headers, tablefmt="grid"))
+# Se o parâmetro geralocalkey for passado, gera o arquivo localKey.txt
+if args.geralocalkey:
+    with open('localKey.txt', 'w') as f:
+        for local_key in local_keys:
+            f.write(local_key + '\n')
+    print("Arquivo localKey.txt gerado com sucesso.")
+else:
+    # Exibe a tabela de dispositivos
+    headers = ["name", "id", "key", "mac", "category", "product_name", "model"]
+    device_list = [[device.get(header, '') for header in headers] for device in devices]
+    device_list_sorted = sorted(device_list, key=lambda x: x[0])
+    print(tabulate(device_list_sorted, headers=headers, tablefmt="grid"))
